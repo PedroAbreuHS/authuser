6bb3fe5853f3eeae433d44e1d0820ceecba98d8e
PedroAbreuHS <104274261+PedroAbreuHS@users.noreply.github.com>
2023-08-15 10:11:59 -0300

    created Models

---
+++ authuser/src/main/java/com/ead/authuser/models/UserModel.java
@@ -0,0 +1,54 @@
+package com.ead.authuser.models;
+
+import com.ead.authuser.enums.UserStatus;
+import com.ead.authuser.enums.UserType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/*Serializabel realiza a conversão adequada para persistir os dados no banco*/
+@Data //A anotação do lombok cria os métodos construtores, get, set, ToString, Equals e HashCode
+@JsonInclude(JsonInclude.Include.NON_NULL) //Para ocultar tributos com valores nulos na serialização para json
+@Entity
+@Table(name = "TB_USERS")
+public class UserModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    @Column(nullable = false, unique = true, length = 50)
+    private String userName;
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+    @Column(nullable = false, length = 255)
+    @JsonIgnore //para não retornar esse campo ao usuário
+    private String password;
+    @Column(nullable = false, length = 150)
+    private String fullName;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+    @Column(length = 20)
+    private String phoneNumber;
+    @Column(length = 20)
+    private String cpf;
+    @Column
+    private String imagUrl;
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime creationDate;
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime lastUpdateDate;
+
+}
