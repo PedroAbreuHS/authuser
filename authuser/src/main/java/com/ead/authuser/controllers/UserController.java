c3eae4fca42d17e15a98e8715e3484ff0e9d99ae
PedroAbreuHS <104274261+PedroAbreuHS@users.noreply.github.com>
2023-08-15 14:16:37 -0300

    new_atualization

---
+++ authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -0,0 +1,54 @@
+package com.ead.authuser.controllers;
+
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/users")
+public class UserController {
+
+    //ponto de injeção para UserService
+    @Autowired
+    UserService userService;
+
+    //Método para listar todos os usuáros
+    @GetMapping
+    public ResponseEntity<List<UserModel>> getAllUsers(){
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
+    }
+
+    //Método para receber um id e retornar
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> getOneUser(@PathVariable(value = "userId")UUID userId){
+
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+        if (!userModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not Found.");
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(userModelOptional.get());
+        }
+    }
+
+    //Método para deletar um id
+    @DeleteMapping("/{userId}")
+    public Object deleteUser(@PathVariable(value = "userId")UUID userId){
+
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+        if (!userModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not Found.");
+        } else {
+            userService.delete(userModelOptional.get());
+            return ResponseEntity.status(HttpStatus.OK).body("User Deleted Success.");
+        }
+    }
+}
--- authuser/src/main/java/com/ead/authuser/models/UserModel.java
+++ authuser/src/main/java/com/ead/authuser/models/UserModel.java
@@ -23,30 +23,41 @@ public class UserModel implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID userId;
+
     @Column(nullable = false, unique = true, length = 50)
     private String userName;
+
     @Column(nullable = false, unique = true, length = 50)
     private String email;
+
     @Column(nullable = false, length = 255)
     @JsonIgnore //para não retornar esse campo ao usuário
     private String password;
+
     @Column(nullable = false, length = 150)
     private String fullName;
+
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private UserStatus userStatus;
+
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
     @Column(length = 20)
     private String phoneNumber;
+
     @Column(length = 20)
     private String cpf;
+
     @Column
     private String imagUrl;
+
     @Column(nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime creationDate;
+
     @Column(nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime lastUpdateDate;
