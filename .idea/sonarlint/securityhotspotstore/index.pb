7baf1230c5a2092aead0e9a76bb750337b7dae71
PedroAbreuHS <104274261+PedroAbreuHS@users.noreply.github.com>
2023-08-25 16:04:34 -0300

    validações

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -5,3 +5,11 @@ w
 G.idea/sonarlint/issuestore/e/d/ed0d1186e36669713d980b0d53777e60dd725b56,9\9\99bf14a94c4cf61f97469637a04c9b816217899e
 Å
 Q.idea/sonarlint/securityhotspotstore/e/d/ed0d1186e36669713d980b0d53777e60dd725b56,d\1\d135e3769b04a72b5ee9501da9fdba1da4580a57
+m
+=authuser/src/main/java/com/ead/authuser/models/UserModel.java,8\e\8e58566fae79f6bd912585456d7d1530c65230e5
+x
+Hauthuser/src/main/java/com/ead/authuser/repositories/UserRepository.java,f\0\f0b88759fc499346c65720aacb17a9c2f2f4001e
+z
+Jauthuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java,2\8\284f99adbb9c55e68464e76dcf5e45e194cacaa8
+q
+Aauthuser/src/main/java/com/ead/authuser/services/UserService.java,a\6\a6471238067fd7d76ea8556fc2f232a65040f017
--- authuser/src/main/java/com/ead/authuser/dtos/UserDto.java
+++ authuser/src/main/java/com/ead/authuser/dtos/UserDto.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 @Data //Método lombok para não ser necessário criar getters e setters
@@ -19,15 +22,23 @@ public class UserDto {
 
     private UUID userId;
 
+    @NotBlank(groups = UserView.RegistrationPost.class)
+    @Size(min = 4, max = 50)
     @JsonView(UserView.RegistrationPost.class)
     private String userName;
 
+    @NotBlank(groups = UserView.RegistrationPost.class)
+    @Email
     @JsonView(UserView.RegistrationPost.class)
     private String email;
 
+    @NotBlank(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
+    @Size(min = 6, max = 20)
     @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
 
+    @NotBlank(groups = UserView.PasswordPut.class)
+    @Size(min = 6, max = 20)
     @JsonView(UserView.PasswordPut.class)
     private String oldPassword;
 
@@ -40,8 +51,8 @@ public class UserDto {
     @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String cpf;
 
+    @NotBlank(groups = UserView.ImagePut.class)
     @JsonView(UserView.ImagePut.class)
     private String imagUrl;
-
 
 }
